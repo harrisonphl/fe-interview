29af733db0455382baefa8c48a84618ac5d2c162
Harry <harry@fumblebee.site>
2023-06-28 18:23:51 -0400

    adds persistence for search context on navigation

--- src/screens/UserSearch.jsx
+++ src/screens/UserSearch.jsx
@@ -1,13 +1,13 @@
-import { ErrorMessage, Field, Form, Formik } from "formik";
-import { useContext, useEffect, useState } from "react";
-import { string, object } from "yup";
-import { API_URL, SortOrder } from "../constants";
-import { useNavigate } from "react-router-dom";
-import { SearchContext } from "../SearchContext";
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import { useContext, useEffect, useState } from 'react'
+import { string, object } from 'yup'
+import { API_URL, SortOrder } from '../constants'
+import { useNavigate } from 'react-router-dom'
+import { SearchContext } from '../SearchContext'
 
-const formInitialValues = { trackingNumber: "" };
+const formInitialValues = { trackingNumber: '' }
 
-const trackingNumberRegex = /^[A-Za-z\d-]*$/;
+const trackingNumberRegex = /^[A-Za-z\d-]*$/
 
 const validationSchema = object().shape({
   trackingNumber: string()
@@ -17,46 +17,46 @@ const validationSchema = object().shape({
         `${label} may only contain alphanumeric characters and dashes (-)`
     )
     .required()
-    .label("Tracking Number"),
-});
+    .label('Tracking Number'),
+})
 
 const UserSearch = () => {
-  const { searchContext, setSearchContext } = useContext(SearchContext);
+  const { searchContext, setSearchContext } = useContext(SearchContext)
 
-  const navigate = useNavigate();
-  // const [searchValue, setSearchValue] = useState(searchContext.searchQuery);
-  // const [searchResult, setSearchResult] = useState(searchContext.searchResult);
+  const navigate = useNavigate()
 
   const getData = () => {
     console.log('getData()')
-    // console.log(searchValue)
     console.log(searchContext.searchQuery)
     fetch(`${API_URL}/api/users?trackingNumber=${searchContext.searchQuery}`)
-    .then(res => res.json())
-    .then(res => {
-      console.log(res)
-      setSearchContext({
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        setSearchContext({
           searchQuery: searchContext.searchQuery,
           searchResult: [...res],
-          originalOrder: [...res]
+          originalOrder: [...res],
         })
-    })
+      })
   }
 
-  const sort = (sortType) => {
-    const oldContext = searchContext
-    if(sortType === SortOrder.DESCENDING){
-      const sortedResult = searchContext.searchResult.sort((a, b) => b.email.localeCompare(a.email));
-      console.log(sortedResult)
-      oldContext.searchResult = [...sortedResult]
-      setSearchContext(oldContext)
-    } else if(sortType === SortOrder.ORIGINAL) {
-      console.log(searchContext)
-      setSearchContext({searchResult: [...searchContext.originalOrder]})
+  const sort = sortType => {
+    const newContext = { ...searchContext }
+    if (sortType === SortOrder.DESCENDING) {
+      const sortedResult = searchContext.searchResult.sort((a, b) =>
+        b.email.localeCompare(a.email)
+      )
+      newContext.searchResult = [...sortedResult]
+      setSearchContext(newContext)
+    } else if (sortType === SortOrder.ORIGINAL) {
+      newContext.searchResult = [...searchContext.originalOrder]
+      setSearchContext(newContext)
     } else {
-      const sortedResult = searchContext.searchResult.sort((a, b) => a.email.localeCompare(b.email));
-      console.log(sortedResult)
-      setSearchContext({searchResult: [...sortedResult]})
+      const sortedResult = searchContext.searchResult.sort((a, b) =>
+        a.email.localeCompare(b.email)
+      )
+      newContext.searchResult = [...sortedResult]
+      setSearchContext(newContext)
     }
   }
 
@@ -72,9 +72,14 @@ const UserSearch = () => {
         <Form>
           <div className="search-input">
             {/* <Field name="trackingNumber" placeholder="Tracking Number" /> */}
-            <input name="trackingNumber" placeholder="Tracking Number" onChange={(e) => {
-              setSearchContext({ searchQuery: e.target.value })
-            }} value={searchContext.searchQuery || ''}/>
+            <input
+              name="trackingNumber"
+              placeholder="Tracking Number"
+              onChange={e => {
+                setSearchContext({ searchQuery: e.target.value })
+              }}
+              value={searchContext.searchQuery || ''}
+            />
             <button type="submit">Search</button>
           </div>
           <div className="search-error">
@@ -83,32 +88,39 @@ const UserSearch = () => {
         </Form>
       </Formik>
       <div>
-        <button onClick={() => sort(SortOrder.ASCENDING)}>Sort Ascending by email</button>
-        <button onClick={() => sort(SortOrder.DESCENDING)}>Sort descending by email</button>
-        <button onClick={() => sort(SortOrder.ORIGINAL)}>Sort orignal by email</button>
+        <button onClick={() => sort(SortOrder.ASCENDING)}>
+          Sort Ascending by email
+        </button>
+        <button onClick={() => sort(SortOrder.DESCENDING)}>
+          Sort descending by email
+        </button>
+        <button onClick={() => sort(SortOrder.ORIGINAL)}>
+          Sort orignal by email
+        </button>
       </div>
       <div className="search-results">
         <table>
           <thead>
             <tr>
-            <th>email</th>
+              <th>email</th>
               <th>first name</th>
               <th>last name</th>
             </tr>
           </thead>
           <tbody>
-            {searchContext.searchResult && searchContext.searchResult.map((item) => (
-              <tr key={item.id} onClick={() => navigate(`/user/${item.id}`)}>
-              <th>{item.email}</th>
-              <th>{item.first}</th>
-              <th>{item.last}</th>
-            </tr>
-            ))}
+            {searchContext.searchResult &&
+              searchContext.searchResult.map(item => (
+                <tr key={item.id} onClick={() => navigate(`/user/${item.id}`)}>
+                  <th>{item.email}</th>
+                  <th>{item.first}</th>
+                  <th>{item.last}</th>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export { UserSearch };
+export { UserSearch }
